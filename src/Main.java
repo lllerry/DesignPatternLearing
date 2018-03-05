7d75f559e7bb2431b01629e9e79525a6e375ebfa
lerry <laijun@znds.com>
2018-03-05 10:00:09 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        simple_factory();
+    }
+
+    /**
+     * 简单工厂模式
+     */
+    private static void simple_factory() {
+
+    }
+
+}
